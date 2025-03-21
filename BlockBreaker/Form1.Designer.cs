355990cfa82abcae44d7a1a777b4b7f36c5534a4
briasari660 <briasari660@SDS-L-F8QLG63>
2025-03-21 14:37:57 -0400

    made form1 longer

--- BlockBreaker/Form1.Designer.cs
+++ BlockBreaker/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(682, 753);
+            this.ClientSize = new System.Drawing.Size(682, 774);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Block Breaker";
